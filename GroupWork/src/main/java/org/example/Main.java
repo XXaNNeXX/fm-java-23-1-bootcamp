d35691cb10378f0f0565fc34fff28f170542250d
neuefische <info@neuefische.de>
2023-08-22 16:40:44 +0200

    new calculation method

--- GroupWork/src/main/java/org/example/Main.java
+++ GroupWork/src/main/java/org/example/Main.java
@@ -4,6 +4,10 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello and welcome!");
+        System.out.println(number(3,6));
+    }
 
+    public static int number (int a, int b) {
+        return a + b;
     }
 }
