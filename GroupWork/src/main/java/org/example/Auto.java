392433b635ec8edb91100e27b1907c7307fc9cb6
neuefische <info@neuefische.de>
2023-08-29 12:30:41 +0200

    initial commit for constructors task

---
+++ GroupWork/src/main/java/org/example/Auto.java
@@ -0,0 +1,35 @@
+package org.example;
+
+public class Auto {
+
+    String brand;
+    String model;
+    String colour;
+    int year;
+
+    public Auto() {
+
+    }
+    public Auto(String brand, String model, String colour, int year) {
+        this.brand = brand;
+        this.model = model;
+        this.colour = colour;
+        this.year = year;
+    }
+
+    public void startCar(int velocity) {
+
+        if(velocity > 0) {
+            System.out.println("Das Auto ist gestartet");
+        } else {
+            System.out.println("Bitte starte das Auto");
+        }
+    }
+
+    public void accelerateCar(int accelerate) {
+
+            accelerate *= 2;
+            System.out.println("Die Geschwindigkeit beträgt " + accelerate + " km/h");
+
+    }
+}
